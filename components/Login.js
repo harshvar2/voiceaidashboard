7404b6dfba69f571ae2ba529012eda8c0e69872e
Harshvardhan Rajdan <harj220@gmail.com>
2021-10-27 19:42:33 +0530

    fix spinner

--- components/Login.js
+++ components/Login.js
@@ -68,7 +68,7 @@ function login() {
                                     {loginError && <p >{loginError}</p>}
                                 </Form>
                             </Row>
-                            <Row className="d-flex align-center mx-md-auto pb-2">
+                            <Row className="d-block align-center mx-md-auto pb-2">
                             {isLoading && <Spinner animation="border" />}
                             </Row>
                         </Col>
