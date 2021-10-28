d98ba7f549bff5a034a5265a3fe2f5f63382714a
Harshvardhan Rajdan <harj220@gmail.com>
2021-10-29 04:49:40 +0530

    fix reponse

--- components/Login.js
+++ components/Login.js
@@ -5,36 +5,39 @@ import styles from '../styles/Login.module.css';
 import { useRouter } from 'next/router'
 import { useEffect, useCallback } from 'react';
 import { verifyCredentials } from '../utils/apiUtils'
-
+import Expire from './Expire'
 import { Row, Col, Container, Form, FloatingLabel, Button, Spinner } from 'react-bootstrap';
 function login() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [loginError, setLoginError] = useState("");
     const [isLoading, setIsLoading] = useState(false)
-    const [loggedIn, setLoggedIn] = useState(false)
+    const [loggedIn, setLoggedIn] = useState()
     const router = useRouter()
-    let token
+ 
     const handleSubmit = async (event) => {
         event.preventDefault();
         setIsLoading(!isLoading)
-        await verifyCredentials(email, password)
-        token = await localStorage.getItem("token")
-        if (token == null) {
+      
+        verifyCredentials(email, password)
+        let token =localStorage.getItem("token")
+        if (token) {
+            setLoggedIn(true)
+            router.push('/home')
+        }
+        else {
             setLoggedIn(false)
             setLoginError("Invalid Username or Password")
         }
-        else {
-            setLoggedIn(true)
-        }
     }
-    useEffect(() => {
-        if (loggedIn == true)
-            return (router.push('/home'))
-        if (token != null) {
-            return (router.push('/home'))
-        }
-    }, [loggedIn])
+    // useEffect(() => {
+    //     if (loggedIn == true)
+    //         return ()
+    //     if (token != null) {
+    //         return (router.push('/home'))
+    //     }
+       
+    // }, [loggedIn,handleSubmit,isLoading])
 
     return (
         <div >
@@ -72,9 +75,11 @@ function login() {
                                     <Button className={styles.signInButton} variant="warning" disabled={!(email.length > 0 && password.length > 0)} type="submit">
                                         <span className={styles.signInButtonText}>SIGN IN</span>
                                     </Button>
-                                    <Row className="d-flex align-center justify-content-center">
+                                    <Expire delay="5000"><Row className="d-flex align-center justify-content-center">
                                         {isLoading && <Spinner animation="border" />}
-                                    </Row>
+                                    </Row></Expire>
+
+                                    
                                     {loginError && <p >{loginError}</p>}
                                 </Form>
                             </Row>
--- utils/apiUtils.js
+++ utils/apiUtils.js
@@ -1,16 +1,15 @@
 
 import axios from 'axios';
 export let verifyCredentials = async (email, password) => {
-    let response 
+    let token ="iojqewjijqwejoij"
     axios.post('https://reqres.in/api/login', {
         email: email,
         password: password,
     })
         .then((res) => {
-            response = res
-            localStorage.setItem("token","iojqewjijqwejoij")
+            localStorage.setItem("token",token)
         }).catch(err => {
         })
 
-    return response
+    return token
 }
